0c739c527f248d525086994799d4a2563706e96a
Lucas Mendes <lucas02.mendes@gmail.com>
2023-03-28 22:15:49 -0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [counter2, setCounter2] = useState(0);
+
+  // componentDidUpdate
+  useEffect(() => {
+    console.log('componentDidUpdate');
+  });
+
+   // componentDidMount
+  useEffect(() => {
+    console.log('componentDidMount');
+  }, []);
+
+  // com dependencia 
+  useEffect(() => {
+    console.log('C1', counter, 'C2', counter2);
+  }, [counter, counter2]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Contador: {counter}</h1>
+      <h1>Contador2: {counter2}</h1>
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+      <button onClick={() => setCounter2(counter2 + 1)}>+ 2</button>
     </div>
   );
 }
