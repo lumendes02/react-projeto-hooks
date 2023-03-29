c0ffaad14f62f8c52c1233e09991008b6ceca020
Lucas Mendes <lucas02.mendes@gmail.com>
2023-03-28 22:50:46 -0300

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
-
+import P from 'prop-types';
 import './App.css';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+
+const Button = React.memo(({incrementButton}) => {
+  console.log('filho')
+  return <button onClick={() => incrementButton(10)}>+</button>
+});
+
+Button.propTypes = {
+  incrementButton: P.func
+}
+
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const [counter2, setCounter2] = useState(0);
 
-  // componentDidUpdate
-  useEffect(() => {
-    console.log('componentDidUpdate');
-  });
-
-   // componentDidMount
-  useEffect(() => {
-    console.log('componentDidMount');
+  const incrementCounter = useCallback((num) => {
+    setCounter((c) => c + num);
   }, []);
 
-  // com dependencia 
-  useEffect(() => {
-    console.log('C1', counter, 'C2', counter2);
-  }, [counter, counter2]);
+  console.log('pai')
 
   return (
     <div className="App">
       <h1>Contador: {counter}</h1>
-      <h1>Contador2: {counter2}</h1>
-      <button onClick={() => setCounter(counter + 1)}>+</button>
-      <button onClick={() => setCounter2(counter2 + 1)}>+ 2</button>
+      <Button incrementButton={incrementCounter}/>
     </div>
   );
 }
