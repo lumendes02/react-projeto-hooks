12d4bb391bd2c680342e8e8fd8e342b592c5a9d3
Lucas Mendes <lucas02.mendes@gmail.com>
2023-05-04 20:48:16 -0300

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import React, { useContext, useEffect, useMemo, useRef, useState } from 'react';
+import React, { useContext, useEffect, useMemo, useReducer, useRef, useState } from 'react';
 import './App.css';
-import { AppContext } from './contexts/AppContext';
-import { Div } from './components/Div';
+
+const globalState = {
+  title: 'O titulos que contexto',
+  body: 'body do contexto',
+  counter: 0,
+}
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'muda':
+      console.log('chamou muda com', action.payload);
+      return {...state, title: action.payload};
+    case 'inverter':
+      console.log('chamou inverter');
+      const {title} = state;
+      return {...state, title: title.split('').reverse().join('')};
+    default:
+      console.log('default');
+  }
+
+  return {...state};
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, globalState);
+  const {counter, title, body} = state;
+
   return (
-    <AppContext>
-      <Div></Div>
-    </AppContext>
+    <div>
+      <h1>{title} {counter}</h1>
+      <button onClick={() => dispatch({
+         type: 'muda',
+         payload: new Date().toLocaleString('pt-BR'),
+        })}>Click</button>
+      <button onClick={() => dispatch({ type: 'inverter' })}>inverter</button>
+    </div>
   )
 }
 
